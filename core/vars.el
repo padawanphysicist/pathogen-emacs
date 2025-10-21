21d11af6dee6686b1d60680c8b82e937b35dc98a
Victor Santos (vct) <victor_santos@fisica.ufc.br>
2025-10-21 20:44:49 -0300

    fix early init

---
+++ core/vars.el
@@ -0,0 +1,55 @@
+;;; vars.el --- Shared configuration variables -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Victor Santos
+;;
+;; Author: Victor Santos <victor_santos@fisica.ufc.br>
+;; Package-Requires: ((emacs 27))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;; This file defines shared configuration variables used across Pathogen Emacs.
+;; It establishes the contract between early-init.el, init.el, and loaded modules.
+;;
+;; This file is loaded by both early-init.el and init.el to ensure variables
+;; are available at all stages of initialization.
+;;
+;;; Code:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Public API Variables
+;;
+;; These variables are safe for users to customize in their personal
+;; configuration files.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defvar pathogen-gc-cons-threshold 67108864 ; 64MB
+  "The default value to use for `gc-cons-threshold' after initialization.
+During startup, GC threshold is set very high to speed up initialization.
+After startup completes, this value is restored for normal operation.
+
+If you experience freezing, decrease this value.
+If you experience stuttering, increase this value.
+
+Default: 64MB (67108864 bytes)")
+
+(defvar pathogen-gc-cons-percentage 0.5 ; 50%
+  "The default value to use for `gc-cons-percentage' after initialization.
+This controls how much heap growth triggers garbage collection.
+
+Default: 0.5 (50% growth triggers GC)")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Internal Variables
+;;
+;; This variables are not supposed to be customized
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defconst pathogen--emacs-dir user-emacs-directory)
+(defconst pathogen--core-dir (expand-file-name "core" pathogen--emacs-dir))
+(defconst pathogen--layers-dir (expand-file-name "layers" pathogen--emacs-dir))
+
+(defvar pathogen--file-name-handler-alist file-name-handler-alist
+  "Backup of file-name-handler-alist for restoration after init.")
+
+(provide 'vars)
+;;; vars.el ends here
--- early-init.el
+++ early-init.el
@@ -1,4 +1,4 @@
-;;; early-init.el --- Emacs 27+ pre-initialization -*- lexical-binding: t; -*-
+;;; early-init.el --- Emacs 27+ pre-initialization -*- no-byte-compile: t; lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2021 Victor Santos
 ;;
@@ -12,144 +12,9 @@
 ;;  Emacs (27+) introduces early-init.el, which is run before init.el, before
 ;;  package and UI initialization happens.
 ;;
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Early-Init-File.html
+;;
 ;;; Code:
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; File name handler optimization
-;;
-;;
-;; Disable file-name-handler-alist during initialization for significant
-;; performance improvement (30-50% faster startup). Handlers check for remote
-;; files, archives, compression, etc., which are not needed during init.
-;; The original list is restored after startup completes.
-(defvar pathogen--file-name-handler-alist file-name-handler-alist
-  "Backup of file-name-handler-alist for restoration after init.")
-(setq file-name-handler-alist nil)
-
-;; Restore after initialization
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (setq file-name-handler-alist pathogen--file-name-handler-alist)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Load shared variables
-;;
-;;
-;; Load pathogen-vars.el which defines shared configuration variables
-;; used across early-init.el, init.el, and loaded modules.
-(load (concat user-emacs-directory "pathogen-vars.el"))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Modify garbage collector
-;;
-;;
-;; The garbage collector (GC) of Emacs in Emacs is very simple. You allocate
-;; some bytes while using it and once you pass a certain threshold, the garbage
-;; collector is triggered.
-;;
-;; It turns out that the default behavior is to garbage collect very often (you
-;; can check whether or not this is case for you by setting the variable
-;; `garbage-collection-messages' to t). Usually there is so little garbage to
-;; collect each time that you will not notice any lag. The problem is when you
-;; use memory-intensive features like `helm' on a large collection.
-;;
-;; GC also can eats up quite a bit of time, easily doubling startup time. We
-;; reduce this initialization time by defering the garbage collector, turning up
-;; the memory threshold as early as possible.
-;;
-;; To control the trigger of the garbage collector we can use the variables
-;; `gc-cons-threshold' and `gc-cons-percentage'.
-;;
-;; Therefore to improve the GC we adopt the following strategy:
-;;
-;;     a. Increase `gc-cons-threshold' to large number so GC is not triggered
-;;        early during startup
-;;     b. Restore it to a sane value after initialization finishes.
-;;
-;; Step 'a' is easy:
-(defun pathogen--defer-gc ()
-  "Defer garbage collection."
-  (setq gc-cons-threshold most-positive-fixnum))
-(pathogen--defer-gc)
-
-;; For step 'b', I proceed as follows: since the default value of
-;; `gc-cons-threshold' is 800000 (800KB), we use the values defined in
-;; pathogen-vars.el (pathogen/gc-cons-threshold and pathogen/gc-cons-percentage).
-
-;; and add a function to restore GC as a hook:
-;;
-;; * `emacs-startup-hook' functions are evaluated later than `after-init-hook'
-;; function, as you can check in
-;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Startup-Summary.html#Startup-Summary):
-(defun pathogen--restore-gc ()
-  "Restore garbage collection."
-  (setq gc-cons-threshold pathogen/gc-cons-threshold)
-  (setq gc-cons-percentage pathogen/gc-cons-percentage))
-(add-hook 'emacs-startup-hook #'pathogen--restore-gc)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Garbage collector within minibuffer
-;;
-;;
-;; We use the same strategy when using minibuffer:
-(add-hook 'minibuffer-setup-hook #'pathogen--defer-gc)
-(add-hook 'minibuffer-exit-hook  #'pathogen--restore-gc)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Garbage collect only when Emacs is out of focus
-;;
-;;
-;; This keeps GC out of your way:
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (add-function :after after-focus-change-function
-                          (lambda ()
-                            (unless (frame-focus-state)
-                              (garbage-collect))))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Improve subprocess communication
-;;
-;;
-;; Increase the amount of data which Emacs reads from subprocesses in a single
-;; chunk. This is especially important for LSP servers and other tools that
-;; communicate via stdout/stdin.
-;;
-;; The default value is 4096 bytes (4KB), which is far too low for modern
-;; systems. Setting this to 1MB significantly improves performance of language
-;; servers, tree-sitter parsers, and other external tools.
-(setq read-process-output-max (* 1024 1024)) ; 1MB
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Early UI optimizations
-;;
-;;
-;; Disable the startup screen to prevent it from flashing briefly during
-;; initialization. Setting this in early-init.el ensures it never appears,
-;; rather than appearing and then being hidden when init.el loads.
-(setq inhibit-startup-screen t)
-
-;; Bell configuration: Flash the mode line instead of the entire screen
-;; or making an audible beep. The default visual bell flashes the whole
-;; screen which can be jarring and distracting. This subtle mode-line
-;; flash provides feedback without being intrusive.
-(setq visible-bell nil)  ; Disable default visual bell
-(setq ring-bell-function
-      (lambda ()
-        (let ((orig-fg (face-foreground 'mode-line)))
-          (set-face-foreground 'mode-line "#F2804F")
-          (run-with-idle-timer 0.1 nil
-                               (lambda (fg) (set-face-foreground 'mode-line fg))
-                               orig-fg))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Improve loading of files
-;;
-;;
-;; In noninteractive sessions, prioritize non-byte-compiled source files to
-;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
-;; to skip the mtime checks on every *.elc file.
-(setq load-prefer-newer 'noninteractive)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Defer package initialization
@@ -160,21 +25,18 @@
 (setq package-enable-at-startup nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Warning level configuration
-;;
-;;
-;; Set to :error instead of :emergency to avoid suppressing important warnings
-;; about configuration issues, deprecated functions, or package loading errors.
-;; This still prevents minor warnings from interrupting startup while keeping
-;; you informed about actual problems.
-;;
-;; Warning levels (least to most severe):
-;;   :debug < :info < :warning < :error < :emergency
-;;
-;; - :emergency suppresses almost everything (previous setting)
-;; - :error shows errors but hides routine warnings (current setting)
-;; - :warning shows all warnings (Emacs default, can be noisy)
-(setq warning-minimum-level :error)
+;;; Add Pathogen directories to `load-path'
+(let ((pathogen-root-dir (file-name-directory (file-truename load-file-name))))
+  (dolist (dir '("core"))
+    (add-to-list 'load-path (expand-file-name dir pathogen-root-dir))))
+
+(require 'vars)
+(require 'early-funcs)
+
+(pathogen/toggle-gui-elements-off)
+(pathogen/tune-garbage-collector)
+(pathogen/ui-tweaks)
+(pathogen/minor-tweaks)
 
 (provide 'early-init)
 ;;; early-init.el ends here
